610a58406e8cd0bb97c0df3438e625c97a44bb04
Prajan17 <prajan4617@gmail.com>
2026-07-13 11:01:21 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,51 @@
-basic.forever(function () {
-	
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    basic.showLeds(`
+        . # # # .
+        # # . # #
+        # . # . #
+        # # . # #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.A, function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Prelude), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.clearScreen()
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Prelude), music.PlaybackMode.InBackground)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # # . # #
+        # . . . #
+        . # # # .
+        `)
 })
